9c36a4d07b91ee0e1f1836870716a1bd55e2d8da
betterchalk-olayeni <olayeni@betterchalk.com>
2021-08-03 13:50:47 +0000

    Created Loopback App

---
+++ easycash/src/__tests__/acceptance/test-helper.ts
@@ -0,0 +1,32 @@
+import {EasycashApplication} from '../..';
+import {
+  createRestAppClient,
+  givenHttpServerConfig,
+  Client,
+} from '@loopback/testlab';
+
+export async function setupApplication(): Promise<AppWithClient> {
+  const restConfig = givenHttpServerConfig({
+    // Customize the server configuration here.
+    // Empty values (undefined, '') will be ignored by the helper.
+    //
+    // host: process.env.HOST,
+    // port: +process.env.PORT,
+  });
+
+  const app = new EasycashApplication({
+    rest: restConfig,
+  });
+
+  await app.boot();
+  await app.start();
+
+  const client = createRestAppClient(app);
+
+  return {app, client};
+}
+
+export interface AppWithClient {
+  app: EasycashApplication;
+  client: Client;
+}
